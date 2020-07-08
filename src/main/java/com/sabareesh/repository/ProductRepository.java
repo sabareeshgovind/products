bf25d5ea026ddb12e3d38553b3d0756c0f348c9d
sabareeshgovind <56748663+sabareeshgovind@users.noreply.github.com>
2020-07-08 12:41:56 -0400

    Added CrossOrigin support

--- src/main/java/com/sabareesh/repository/ProductRepository.java
+++ src/main/java/com/sabareesh/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.sabareesh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.sabareesh.entity.Product;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
 
