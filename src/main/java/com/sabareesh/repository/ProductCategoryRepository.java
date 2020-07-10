90cdc7c1d0f7744e685276b4c5f302d37f079af7
sabareeshgovind <56748663+sabareeshgovind@users.noreply.github.com>
2020-07-10 11:04:51 -0400

    added db script

--- src/main/java/com/sabareesh/repository/ProductCategoryRepository.java
+++ src/main/java/com/sabareesh/repository/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.sabareesh.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.sabareesh.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
 
 }
